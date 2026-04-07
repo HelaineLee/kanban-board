7b147f9944f64d49ff91c3fe36d8cfae818b60b9
HelaineLee <puppleowl@naver.com>
2026-04-08 00:51:17 +0900

    feat : change from accelerate url to adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as typeof globalThis & {
   prisma?: PrismaClient;
 };
 
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+const adapter = new PrismaPg(pool)
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    accelerateUrl: process.env["DATABASE_URL"],
+    adapter
   } as unknown as ConstructorParameters<typeof PrismaClient>[0]);
 
 if (process.env.NODE_ENV !== "production") {
